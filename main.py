12a16c9940fc5e1d1416d222fac5a90d7ea4fb89
Nitin Solanki <nitinmlvya@gmail.com>
2022-02-08 21:36:37 +0530

    test api

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+import time
+import json
+from random import randint
+import threading
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
 data = [{"Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, { "Gender": "Male", "HeightCm": 161, "WeightKg": 85 }, { "Gender": "Male", "HeightCm": 180, "WeightKg": 77 }, { "Gender": "Female", "HeightCm": 166, "WeightKg": 62}, {"Gender": "Female", "HeightCm": 150, "WeightKg": 70}, {"Gender": "Female", "HeightCm": 167, "WeightKg": 82}]
+
+data_json = []
+for i in range(0, 100000):
+	gender = "Male" if i%2==0 else "Female"
+	height = num1= randint(120, 200)
+	weight = num1= randint(30, 200)
+	data_json.append({"Gender": gender, "HeightCm": height, "WeightKg": weight})
+
+out_file = open("input_data.json", "w")
+json.dump(data_json, out_file, indent=2)
+
 
 health_risk_bmi_data = {
 	"BMI_category": ["Underweight", "Normal weight", "Overweight", "Moderately obese", "Severely obese", "Very severely obese"],
 	"BMI_range": [(None, 18.4), (18.4, 24.9), (24.9, 29.9), (29.9, 34.9), (34.9, 39.9), (39.9, None)],
 	"health_risk": ["Malnutrition risk", "Low risk", "Enhanced risk", "Medium risk", "High risk", "Very high risk"]
 }
+
 
 
 class Solution():
@@ -34,6 +56,7 @@ class Solution():
 				if bmi > bmi_range[0] and bmi <= bmi_range[1]:
 					return health_risk_bmi_data['health_risk'][i], health_risk_bmi_data['BMI_category'][i]
 
+
 	def run(self, weight_in_kg=75, height_in_cm=175):
 		bmi = self.calculate_bmi(weight_in_kg, height_in_cm)
 		health_risk, bmi_category = self.calcuate_health_risk(bmi)
@@ -41,13 +64,37 @@ class Solution():
 			self.overweight_persons_count += 1
 		print(f'BMI: {bmi} & Health Risk: {health_risk}, & BMI Category: {bmi_category}')
 
-if __name__=='__main__':
-	# Solution().run()
+
+
+@app.route('/', methods = ['POST'])
+def hello_world():
+	start_time = time.time()
 	sol = Solution()
-	for person in data:
-		sol.run(weight_in_kg=person['WeightKg'], height_in_cm=person['HeightCm'])
-		print()
-	print(f'Overweight Persons: {sol.overweight_persons_count}')
+	sol.run()
+	end_time = time.time()
+	print(f'Time Taken to complete the execution: {round(end_time - start_time, 2)}')
+	return str(sol.overweight_persons_count)
+
+
+if __name__=='__main__':
+	app.run()
+	# # Solution().run()
+	# start_time = time.time()
+	# sol = Solution()
+	# for person in data_json:
+	# 	sol.run(weight_in_kg=person['WeightKg'], height_in_cm=person['HeightCm'])
+	# # threads = []
+	# # for person in data_json:
+	# # 	x = threading.Thread(target=sol.run, args=(person['WeightKg'],person['HeightCm']))
+	# # 	threads.append(x)
+	# # 	x.start()
+	# # 	# sol.run(weight_in_kg=person['WeightKg'], height_in_cm=person['HeightCm'])
+
+	# # for index, thread in enumerate(threads):
+	# # 	thread.join()
+	# print(f'Overweight Persons: {sol.overweight_persons_count}')
+	# end_time = time.time()
+	# print(f'Time Taken to complete the execution: {round(end_time - start_time, 2)}')
 	
 
 
