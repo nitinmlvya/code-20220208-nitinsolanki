ff2967141c360f83750ddc2ced9d1507ad000b23
Nitin Solanki <nitinmlvya@gmail.com>
2022-02-09 07:11:34 +0530

    Modified the code

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
 import time
 import json
-from random import randint
-import threading
+import pandas as pd
+from typing import List, Dict, Tuple
+from tqdm import tqdm
 from flask import Flask
 
 app = Flask(__name__)
-
-
-
-data = [{"Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, { "Gender": "Male", "HeightCm": 161, "WeightKg": 85 }, { "Gender": "Male", "HeightCm": 180, "WeightKg": 77 }, { "Gender": "Female", "HeightCm": 166, "WeightKg": 62}, {"Gender": "Female", "HeightCm": 150, "WeightKg": 70}, {"Gender": "Female", "HeightCm": 167, "WeightKg": 82}]
-
-data_json = []
-for i in range(0, 100000):
-	gender = "Male" if i%2==0 else "Female"
-	height = num1= randint(120, 200)
-	weight = num1= randint(30, 200)
-	data_json.append({"Gender": gender, "HeightCm": height, "WeightKg": weight})
-
-out_file = open("input_data.json", "w")
-json.dump(data_json, out_file, indent=2)
 
 
 health_risk_bmi_data = {
@@ -29,21 +16,36 @@ health_risk_bmi_data = {
 
 
 
-class Solution():
-	def __init__(self):
+class BMICalculator():
+	def __init__(self) -> None:
+		self.input_json_file = 'test_data.json'
+		self.output_csv_file = 'test_output.csv'
+
+		self.person_bmi_details = {'BMI': [], 'health_risk': [], 'BMI_category': []}
 		self.overweight_persons_count = 0
 
-	def __convert_to_meter(self, value):
+
+	def __convert_to_meter(self, value: float) -> float:
+		"""
+		Method to convert centimeter to meter
+		"""
 		return value * 0.01
 
 
-	def calculate_bmi(self, weight_in_kg, height_in_cm):
+	def calculate_bmi(self, weight_in_kg: int, height_in_cm: int) -> float:
+		"""
+		Method to calcuate the BMI value based on weight & height.
+		"""
 		height_in_m = self.__convert_to_meter(height_in_cm)
 		bmi = weight_in_kg / (height_in_m * height_in_m)
 		return round(bmi, 2)
 
 
-	def calcuate_health_risk(self, bmi):
+	def get_health_risk_and_bmi_category(self, bmi: float) -> Tuple[str, str]:
+		"""
+		Method to retrieve the health risk & BMI category based on BMI value.
+
+		"""
 		list_of_bmi_range = health_risk_bmi_data['BMI_range']
 		for i, bmi_range in enumerate(list_of_bmi_range):
 			if i == 0:
@@ -57,44 +59,44 @@ class Solution():
 					return health_risk_bmi_data['health_risk'][i], health_risk_bmi_data['BMI_category'][i]
 
 
-	def run(self, weight_in_kg=75, height_in_cm=175):
-		bmi = self.calculate_bmi(weight_in_kg, height_in_cm)
-		health_risk, bmi_category = self.calcuate_health_risk(bmi)
-		if bmi_category == health_risk_bmi_data['BMI_category'][2]:
-			self.overweight_persons_count += 1
-		print(f'BMI: {bmi} & Health Risk: {health_risk}, & BMI Category: {bmi_category}')
+	def save_details(self):
+		df = pd.DataFrame(self.person_bmi_details)
+		df.to_csv(self.output_csv_file, index=False)
+		print(f'Output saved to file : {self.output_csv_file}')
 
+
+	def run(self) -> None:
+		"""
+		Main method to run the program and count the overweighted persons.
+		"""
+		print(f'Reading person details from file: {self.input_json_file}')
+		data_json = json.load(open(self.input_json_file))
+
+		for person in tqdm(data_json, desc ="Calculating BMI..."):
+			bmi = self.calculate_bmi(person['WeightKg'], person['HeightCm'])
+			health_risk, bmi_category = self.get_health_risk_and_bmi_category(bmi)
+
+			self.person_bmi_details['BMI'].append(bmi)
+			self.person_bmi_details['health_risk'].append(health_risk)
+			self.person_bmi_details['BMI_category'].append(bmi_category)
+
+			if bmi_category == health_risk_bmi_data['BMI_category'][2]:
+				self.overweight_persons_count += 1
+
+		self.save_details()
 
 
 @app.route('/', methods = ['POST'])
 def hello_world():
-	start_time = time.time()
-	sol = Solution()
-	sol.run()
-	end_time = time.time()
-	print(f'Time Taken to complete the execution: {round(end_time - start_time, 2)}')
-	return str(sol.overweight_persons_count)
+	calc = BMICalculator()
+	calc.run()
+	return str(calc.overweight_persons_count)
+
 
 
 if __name__=='__main__':
 	app.run()
-	# # Solution().run()
-	# start_time = time.time()
-	# sol = Solution()
-	# for person in data_json:
-	# 	sol.run(weight_in_kg=person['WeightKg'], height_in_cm=person['HeightCm'])
-	# # threads = []
-	# # for person in data_json:
-	# # 	x = threading.Thread(target=sol.run, args=(person['WeightKg'],person['HeightCm']))
-	# # 	threads.append(x)
-	# # 	x.start()
-	# # 	# sol.run(weight_in_kg=person['WeightKg'], height_in_cm=person['HeightCm'])
-
-	# # for index, thread in enumerate(threads):
-	# # 	thread.join()
-	# print(f'Overweight Persons: {sol.overweight_persons_count}')
-	# end_time = time.time()
-	# print(f'Time Taken to complete the execution: {round(end_time - start_time, 2)}')
+	# BMICalculator().run()
 	
 
 
--- tests/test_cases.py
+++ tests/test_cases.py
@@ -1,5 +1,26 @@
 import pytest
 
+from main import BMICalculator
 
-def test_sample():
-    assert 1 == 1
+
+def test_calculate_bmi():
+	bmi = BMICalculator().calculate_bmi(weight_in_kg=75, height_in_cm=175)
+	assert bmi == 24.49
+
+
+def test_health_risk_and_bmi_category():
+	health_risk, bmi_category = BMICalculator().get_health_risk_and_bmi_category(bmi=24.49)
+	assert health_risk == 'Low risk'
+	assert bmi_category == 'Normal weight'
+
+
+def test_result():
+	person_data = [{"Gender": "Male", "HeightCm": 175, "WeightKg": 75 }]
+	person = person_data[0]
+	calc = BMICalculator()
+	bmi = calc.calculate_bmi(person['WeightKg'], person['HeightCm'])
+	health_risk, bmi_category = calc.get_health_risk_and_bmi_category(bmi=bmi)
+
+	assert bmi == 24.49
+	assert health_risk == 'Low risk'
+	assert bmi_category == 'Normal weight'
